236e1ea83fc6ea636819640365a0e7d1c108b200
Dalton Menezes <daltonmenezes@outlook.com>
2020-03-25 21:31:33 -0300

    Update knexfile

--- server/knexfile.js
+++ server/knexfile.js
@@ -30,19 +30,7 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    ...this.development
   }
 
 };
