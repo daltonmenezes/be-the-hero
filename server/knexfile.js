2c76d27f18dddacf3472fa72bd6aac047e4771d4
Dalton Menezes <daltonmenezes@outlook.com>
2020-03-25 21:56:59 -0300

    Update knexfile

--- server/knexfile.js
+++ server/knexfile.js
@@ -30,7 +30,13 @@ module.exports = {
   },
 
   production: {
-    ...this.development
+    client: 'sqlite3',
+    connection: {
+      filename: resolve(__dirname, 'src', 'database', 'db.sqlite')
+    },
+    migrations: {
+      directory: resolve(__dirname, 'src', 'database', 'migrations')
+    },
+    useNullAsDefault: true
   }
-
 };
