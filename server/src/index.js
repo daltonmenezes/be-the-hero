3a5b3ff4292c67309de77642ffe7c48330a8e636
Dalton Menezes <daltonmenezes@outlook.com>
2020-03-28 00:01:59 -0300

    Extract server to app.js separating from app.listen to be more testable

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,4 @@
 require('dotenv').config()
-
-const { errors } = require('celebrate')
-const express = require('express')
-const cors = require('cors')
-const routes = require('./routes')
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-app.use(routes)
-app.use(errors())
+const app = require('./app')
 
 app.listen(process.env.PORT || 3333)
